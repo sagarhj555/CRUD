c5f0323aceedc8fddd349bf91d410fc564ce6999
sagar h <sagarjamadar1995@gmail.com>
2024-06-13 16:53:58 +0530

    Added OpenApi/Swagger Annotations.

--- src/main/java/com/example/WebApplication/Controller/crudController.java
+++ src/main/java/com/example/WebApplication/Controller/crudController.java
@@ -3,6 +3,10 @@ package com.example.WebApplication.Controller;
 import com.example.WebApplication.Model.StudDetails;
 import com.example.WebApplication.Services.crudService;
 import com.example.WebApplication.Services.crudJpaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +22,21 @@ public class crudController {
     @Autowired
     crudJpaService crJs;
 
-
+    @Operation(summary = "To Greet Students")
     @GetMapping("/")
     public String greet() {
         return "Welcome Students";
     }
 
+    @Operation(summary = "This will get all the records from DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+            description = "Fetched all the records",
+            content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404",
+                    description = "No Records",
+                    content = @Content)
+    })
     @GetMapping("/all")
     public List<StudDetails> getAllRecords() {
         return crJs.studData();
